91184c42adb80673c51542bafa5d05b8b67d3c27
LoreGerm <lorenzogermano146@gmail.com>
2022-05-28 19:05:46 +0200

    .

--- ClientServer_validazione/Client.py
+++ ClientServer_validazione/Client.py
@@ -1,17 +1,26 @@
 from threading import Thread
 import zmq
+import socket
+import json
 
 context = zmq.Context()
-socket = context.socket(zmq.REQ)
-socket.connect("tcp://localhost:5555")
+sc = context.socket(zmq.REQ)
+sc.connect("tcp://localhost:5555")
 
-socket.send_string("due")
-message = socket.recv_string()
+ctx = {
+    'azione': 'connetti',
+    'mess': socket.gethostbyname(socket.gethostname()),
+}
+
+ctx = json.dumps(ctx)
+sc.send_string('ctx')
+'''
+message = sc.recv_string()
 print(message)
 
 socket.send_string("porta")
 porta = socket.recv_string()
-
+'''
 def connessioneServer(porta):
     context = zmq.Context()
     socket = context.socket(zmq.REQ)
@@ -21,5 +30,5 @@ def connessioneServer(porta):
     message = socket.recv_string()
     print(message)
 
-t = Thread(target=connessioneServer, args=(porta,))
-t.start()
+    t = Thread(target=connessioneServer, args=(porta,))
+    t.start()
--- ClientServer_validazione/Server.py
+++ ClientServer_validazione/Server.py
@@ -1,4 +1,5 @@
-from email import message
+
+import json
 import zmq
 
 context = zmq.Context()
@@ -10,15 +11,8 @@ socket.bind("tcp://*:5555")
 x = 0
 while x < 3:
     message = socket.recv_string()
-    if message == "porta":
-        print("spedito alla porta")
-        socket.send_string("8000")
-        
-    if message == 'due':
-        socket.send_string("ti connetto")
-        print(message)
-    
-    if message == 'uno':
-        socket.send_string("aspetta")
-        print(message)
+    #message = json.loads(message)
+    print(message)
+    if message['azione'] == 'connetti':
+        print('conneso')
     x += 1
