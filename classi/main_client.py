2ecef651e672385544b8900c3d9ca587f1d5feb0
LoreGerm <lorenzogermano146@gmail.com>
2022-05-31 11:45:02 +0200

    .

--- classi/main_client.py
+++ classi/main_client.py
@@ -9,12 +9,12 @@ giocatore = Giocatore('Paolo', 1000)
 
 giocatore.connetti_casino()
 
+'''
 tavoli = giocatore.ricevi_lita_tavoli()
 
 for t in tavoli:
     print(t)
 
-'''
 t = Thread(target=giocatore.connetti_tavolo, args=(giocatore.connetti_casino(),))
 t.start()
 '''
