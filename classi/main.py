86be76d458ea78cc6c14bf8cf710a55abd022aeb
LoreGerm <lorenzogermano146@gmail.com>
2022-05-31 09:48:22 +0200

    .

--- classi/main.py
+++ classi/main.py
@@ -1,8 +1,13 @@
 from threading import Thread
+from classi.banco import Banco
+from classi.casino import Casino
 from classi.giocatore import Giocatore
 
 
-cl1 = Giocatore('Nikola', 1000)
+
+casino = Casino()
+banco = Banco(52)
+cl1 = Giocatore('Paolo', 1000)
 
 
 t = Thread(target=cl1.connetti_tavolo, args=(cl1.connetti_casino(),))
